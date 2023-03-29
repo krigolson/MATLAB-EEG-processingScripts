af3c48108900f398bbcd5dd2c1b419c9c30aeac9
mathewhammerstrom <50024894+mathewhammerstrom@users.noreply.github.com>
2023-03-29 11:39:05 -0700

    NewAnalysisCode
    Simplified and added descriptors

--- doRemoveEyeComponents.m
+++ doRemoveEyeComponents.m
@@ -2,7 +2,7 @@ function EEG = doRemoveEyeComponents(EEG)
 
     [EEG] = iclabel(EEG, 'default'); %Automatic Inverse ICA
     eyeLabel = find(strcmp(EEG.etc.ic_classification.ICLabel.classes,'Eye')); %Find the components classified as blinks
-    eyeI = find(EEG.etc.ic_classification.ICLabel.classifications(:,eyeLabel)>0.8); %Index the components most likely to be blinks
+    eyeI = find(EEG.etc.ic_classification.ICLabel.classifications(:,eyeLabel)>0.6); %Index the components most likely to be blinks
     [EEG] = pop_subcomp(EEG,eyeI,0); %Remove/subtract the eyeblink components from the data
     
 end
--- singleSubjectERP.m
+++ singleSubjectERP.m
@@ -1,13 +1,19 @@
-% KLab ERP ActiChamp Processing Script by Mat and Olav
+%% KLab ERP ActiChamp Processing Script by Mat and Olav
+% Used to compare effects of different preprocessing pipelines on EEG data
+% quality. 
 
+% Simply pass in data from the eyes-open eyes-closed task, input which
+% pipeline you want to run, and compare measures of data quality. 
+
+%% Prepare Data
 % clear memory, close all windows, clear command window
 clear
 close all
 clc
 
 % specifcy the path to the data and the data filename
-pathName = '/Users/krigolson/Desktop/Cognitive_Assessment_Baseline';
-fileName = 'Cognitive_Assessment_Baseline_1040.vhdr';
+pathName = '/Users/mathewhammerstrom/Documents/GitHub/MATLAB-EEG-processingScripts/Cognitive_Assessment_Baseline';
+fileName = 'Cognitive_Assessment_Baseline_1042.vhdr';
 
 % change to location where data is
 cd(pathName);
@@ -15,71 +21,80 @@ cd(pathName);
 % load the data
 EEG = doLoadBVData(pathName,fileName);
 
-%[EEG1, output, time] = doPrepPipeline(EEG);
+% Determine start and stop points of EO and EC
+for counter = 1:length(EEG.event)
+    if strcmp(EEG.event(counter).type,'S  1')
+        eyesOpenStart = EEG.event(counter).latency;
+    elseif strcmp(EEG.event(counter).type,'S  3')
+        eyesCloseStart = EEG.event(counter).latency;
+    elseif strcmp(EEG.event(counter).type,'S  2')
+        eyesOpenEnd = EEG.event(counter).latency;   
+    elseif strcmp(EEG.event(counter).type,'S  4')
+        eyesCloseEnd = EEG.event(counter).latency;
+    end
+end
 
-EEG1 = EEG;
+%% Pre-process Data: Pipeline Comparison
+% Determine which analysis pipeline you want to run:
 
-EEG2 = EEG1;
+pipeLine = 1;
 
-%EEG = doFilter(EEG,0.1,30,2,60,EEG.srate);
-%EEG1 = doFilter(EEG1,0.1,30,2,60,EEG1.srate);
-%EEG2 = doFilter(EEG2,0.1,30,2,60,EEG2.srate);
+if pipeLine == 1 % "Traditional" pipeline
+    [EEG] = doRereference(EEG,{'TP9','TP10'},'ALL',EEG.chanlocs);  %Rereference to mastoids
+    [EEG] = doFilter(EEG,0.1,30,4,60,250); %Filter data
+    
+    [EEG] = doICA(EEG,1); % Run a fast ICA
+    [EEG] = doRemoveEyeComponents(EEG); %Use IC Label to remove blinks
 
-%EEG = doRereference(EEG,{'TP9','TP10'},{'ALL'},EEG.chanlocs);
-%EEG1 = doRereference(EEG1,{'TP9','TP10'},{'ALL'},EEG1.chanlocs);
-%EEG2 = doRereference(EEG2,{'TP9','TP10'},{'ALL'},EEG2.chanlocs);
+elseif pipeLine == 2 % Simplified PREP Pipeline
+    [EEG, output, time] = doPrepPipeline(EEG); %PREP does its own noise removal and rereferencing
+    
+    [EEG] = doICA(EEG,1);
+    [EEG] = doRemoveEyeComponents(EEG);
 
-EEG = doSegmentData(EEG,{'S202','S203'},[-200 800]);
-EEG1 = doSegmentData(EEG1,{'S202','S203'},[-200 800]);
-EEG2 = doSegmentData(EEG2,{'S202','S203'},[-1000 2000]);
+elseif pipeLine ==3 % Add in a filter before a PREP Pipeline
+    [EEG] = doFilter(EEG,0.1,30,4,60,250); % Compare what happens when you filter before PREP
+    [EEG] = doICA(EEG,1);
+    [EEG] = doRemoveEyeComponents(EEG);
+    [EEG, output, time] = doPrepPipeline(EEG);
+end
 
-EEG2 = doICA(EEG2,1);
-EEG2 = doRemoveEyeComponents(EEG2);
+%% Pre-process Data: Normal steps for post-noise/blink removal
+% Open up two variables for eyes open and eyes closed
+EEGEO = EEG;
+EEGEC = EEG;
 
-EEG2 = doSegmentData(EEG2,{'S202','S203'},[-200 800]);
+% Alter the EEG data to only include Eyes Open (EO)
+EEGEO.data(:,eyesOpenEnd:end) = []; %Remove all data after end of EO
+EEGEO.data(:,1:eyesOpenStart) = []; %Remove all data before EO
+EEGEO.pnts = size(EEGEO.data,2); %Fix the pnts variable to reflect the new data dimensions
+EEGEO.xmax = 1/EEGEO.srate*EEGEO.pnts; %Fix the xmax variable
 
-EEG = doBaseline(EEG,[-200 0]);
-EEG1 = doBaseline(EEG1,[-200 0]);
-EEG2 = doBaseline(EEG2,[-200 0]);
+% Complete the pre-processing steps 
+[EEGEO] = doTemporalEpochs(EEGEO,1000,100); % Create multiple one-second epochs 
+[EEGEO] = doArtifactRejection(EEGEO,'Difference',150); % Artifact rejection, only using difference here
+[EEGEO] = doRemoveEpochs(EEGEO,EEGEO.artifact.badSegments,0); % Remove components 
+[FFTEO] = doFFT(EEGEO,{'S  1'}); % Run FFT on new segments you created 
 
-EEG = doArtifactRejection(EEG,'Difference',150);
-EEG1 = doArtifactRejection(EEG1,'Difference',150);
-EEG2 = doArtifactRejection(EEG2,'Difference',150);
+% Repeat the above steps for Eyes Closed (EC)
+EEGEC.data(:,eyesCloseEnd:end) = [];
+EEGEC.data(:,1:eyesCloseStart) = [];
+EEGEC.pnts = size(EEGEC.data,2);
+EEGEC.xmax = 1/EEGEC.srate*EEGEC.pnts;
 
-EEG = doRemoveEpochs(EEG,EEG.artifact.badSegments,0);
-EEG1 = doRemoveEpochs(EEG1,EEG1.artifact.badSegments,0);
-EEG2 = doRemoveEpochs(EEG2,EEG2.artifact.badSegments,0);
+[EEGEC] = doTemporalEpochs(EEGEC,1000,100);
+[EEGEC] = doArtifactRejection(EEGEC,'Difference',150);
+[EEGEC] = doRemoveEpochs(EEGEC,EEGEC.artifact.badSegments,0);
+[FFTEC] = doFFT(EEGEC,{'S  1'});
 
-ERP = doERP(EEG,{'S202','S203'},0);
-ERP1 = doERP(EEG1,{'S202','S203'},0);
-ERP2 = doERP(EEG2,{'S202','S203'},0);
-
-time = ERP.times;
-
-subplot(2,3,1);
-plot(time,ERP.data(13,:,1));
+% Plot the data at channel Pz to check alpha effect
+plot(1:30,FFTEO.data(13,1:30))
 hold on;
-plot(time,ERP.data(13,:,2));
-
-subplot(2,3,2);
-plot(time,ERP1.data(13,:,1));
+plot(1:30,FFTEC.data(13,1:30))
 hold on;
-plot(time,ERP1.data(13,:,2));
+legend;
 
-subplot(2,3,3);
-plot(time,ERP2.data(13,:,1));
-hold on;
-plot(time,ERP2.data(13,:,2));
-
-subplot(2,3,4);
-bar(EEG.channelArtifactPercentages);
-
-subplot(2,3,5);
-bar(EEG1.channelArtifactPercentages);
-
-subplot(2,3,6);
-bar(EEG2.channelArtifactPercentages);
-
-
-
+%% Compare data quality 
+chanArtifacts(:,1) = EEGEC.channelArtifactPercentages(:,:);
+chanArtifacts(:,2) = EEGEO.channelArtifactPercentages(:,:);
 
